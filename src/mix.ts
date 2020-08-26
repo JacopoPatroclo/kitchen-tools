dbee248f716ed9815ab38e85f55593968ce25230
Jacopo Martinelli <jacopo.patroclo.martinelli@gmail.com>
2020-08-26 19:35:22 +0200

    add kompose

--- src/mix.ts
+++ src/mix.ts
@@ -4,7 +4,19 @@ import { ConfigurationHelper } from "./shared/helpers/ConfigurationHelper";
 import * as yaml from "js-yaml";
 import { join, resolve } from "path";
 import { readFileSync, writeFileSync } from "fs";
-import { merge } from 'lodash'
+import { merge } from "lodash";
+import { spawnSync } from "child_process";
+
+const platform = process.platform;
+const komposePath = resolve(
+  join(
+    __dirname,
+    "..",
+    "bin",
+    "kompose",
+    `kompose-${platform === "win32" ? `${platform}.exe` : platform}`
+  )
+);
 
 let workspaceConfig = null;
 try {
@@ -19,41 +31,78 @@ try {
 const config = new ConfigurationHelper(JSON.stringify(workspaceConfig));
 const basePath = process.cwd();
 const baseDockerCompose = {
-  version: '3.5'
-}
-const dockerComposeProdPath = join(basePath, 'docker-compose.prod.yaml')
+  version: "3.5",
+};
+const dockerComposeProdPath = join(basePath, "docker-compose.prod.yaml");
 
 console.log(`Generating production files for ${config.projectName}`);
 
 // Get all the project docker-composes
-const dockerComposes = config.services.map(service => {
-  const dcContent = readFileSync(join(basePath, service.dcompose), 'utf8').toString()
-  const dockerCompose = yaml.safeLoad(dcContent) as any
-  return { 
+const dockerComposes = config.services.map((service) => {
+  const dcContent = readFileSync(
+    join(basePath, service.dcompose),
+    "utf8"
+  ).toString();
+  const dockerCompose = yaml.safeLoad(dcContent) as any;
+  return {
     ...dockerCompose,
     services: Object.keys(dockerCompose.services)
-      .map(service => ({key: service, config: dockerCompose.services[service]}))
-      .map(dc => {
+      .map((service) => ({
+        key: service,
+        config: dockerCompose.services[service],
+      }))
+      .map((dc) => {
         // Remove all the build configurations
-        const conf = { ...dc.config }
-        conf.build = null
-        delete conf.build
+        const conf = { ...dc.config };
+        conf.build = null;
+        delete conf.build;
         // Remove all the volumes that are referring to the current file structure
-        conf.volumes = conf.volumes?.filter((volume: string) => !volume.includes('./services'))
+        conf.volumes = conf.volumes?.filter(
+          (volume: string) => !volume.includes("./services")
+        );
         // Remove dependency form proxy
-        conf.depends_on = conf.depends_on?.filter(dep => dep !== 'proxy' && dep !== 'dockergen') || []
+        conf.depends_on =
+          conf.depends_on?.filter(
+            (dep) => dep !== "proxy" && dep !== "dockergen"
+          ) || [];
         if (conf.depends_on.length <= 0) {
-          delete conf.depends_on
+          delete conf.depends_on;
         }
-        return { ...dc, config: conf }
+        return { ...dc, config: conf };
       })
-      .reduce(((acc, obj) => ({ ...acc, [obj.key]: obj.config })), {})
-  }
-})
+      .reduce((acc, obj) => ({ ...acc, [obj.key]: obj.config }), {}),
+  };
+});
 
-console.log(`Target project are ${config.services.map(service => service.name).join(', ')}`);
+console.log(
+  `Target project are ${config.services
+    .map((service) => service.name)
+    .join(", ")}`
+);
 
 // Merge in one docker-compose all the selected services (without the proxy stuff)
-const mergedDockerCompose = dockerComposes.reduce((acc, dc) => merge(acc, dc), baseDockerCompose)
+const mergedDockerCompose = dockerComposes.reduce(
+  (acc, dc) => merge(acc, dc),
+  baseDockerCompose
+);
 
-writeFileSync(dockerComposeProdPath, yaml.safeDump(mergedDockerCompose))
+writeFileSync(dockerComposeProdPath, yaml.safeDump(mergedDockerCompose));
+
+spawnSync(
+  komposePath,
+  [
+    "convert",
+    "-f",
+    dockerComposeProdPath,
+    '-o kube.yml',
+    ...process.argv.slice(2, process.argv.length),
+  ],
+  {
+    cwd: process.cwd(),
+    shell: true,
+    env: {
+      ...process.env,
+    },
+    stdio: [process.stdin, process.stdout, process.stderr],
+  }
+);
